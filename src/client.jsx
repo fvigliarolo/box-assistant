3695dd82f13438be0aef8fd54cfc2866e1793834
fvigliarolo <fd.vigliarolo@gmail.com>
2024-09-10 22:09:41 -0300

    variables de entorno para supabase

--- src/client.jsx
+++ src/client.jsx
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const url = "https://wrmmrhqxcedukldtoaqd.supabase.co";
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndybW1yaHF4Y2VkdWtsZHRvYXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwODEwMzMsImV4cCI6MjA0MDY1NzAzM30.jhfiVb2Fnt0caJccWjfnjEeHbWt2ZddgLS2mNVMqg1o"
+
+
+const url = import.meta.env.VITE_SUPABASE_URL;
+const apiKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(url, apiKey);
